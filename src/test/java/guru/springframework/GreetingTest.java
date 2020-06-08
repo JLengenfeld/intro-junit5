82ceb1af32b4e216d75aea1a73901e610b82aaa6
Jan Lengenfeld <coding.jbl@gmail.com>
2020-06-08 20:50:02 +0200

    add before all and after each and after all

--- src/test/java/guru/springframework/GreetingTest.java
+++ src/test/java/guru/springframework/GreetingTest.java
@@ -1,13 +1,15 @@
 package guru.springframework;
 
-import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.Test;
-
-import static org.junit.jupiter.api.Assertions.*;
+import org.junit.jupiter.api.*;
 
 class GreetingTest {
 
     private Greeting greeting;
+
+    @BeforeAll
+    public static void beforeClass(){
+        System.out.println("Before - I am only called Once!!!");
+    }
 
     @BeforeEach
     void setUp() {
@@ -18,10 +20,26 @@ class GreetingTest {
     @Test
     void helloWorld() {
         System.out.println(greeting.helloWorld());
+
     }
 
     @Test
-    void helloWorldWithName() {
+    void helloWorld1() {
         System.out.println(greeting.helloWorld("John"));
+    }
+
+    @Test
+    void helloWorld2() {
+       System.out.println(greeting.helloWorld("Sam"));
+    }
+
+    @AfterEach
+    void tearDown() {
+        System.out.println("In After Each........");
+    }
+
+    @AfterAll
+    public static void afterClass(){
+        System.out.println("After!!! ***** - I am only called Once!!!");
     }
 }
